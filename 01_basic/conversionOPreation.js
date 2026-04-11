18d0e295517e6de0b3a1dad3e1ea069530520fab
ap9988029-debug <ap9988029@gmail.com>
2026-04-11 08:32:59 +0000

    done

--- 01_basic/conversionOPreation.js
+++ 01_basic/conversionOPreation.js
@@ -37,3 +37,33 @@ console.log(stringindata)
     null        =>"null"
     undefined   =>"undefined"
 */
+// -----------------------opreations------------------------------------------------
+let va =2
+let neg_data=-va
+// console.log(neg_data)
+// console.log(2+2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(2-1)
+// console.log(1/3)
+// let a=" ankit"
+// let b="hellow"
+// console.log(b + a)
+console.log("1"+1+3) //if =>first number is string  then =>js consider all opreations as string
+console.log(1+3+"3")//if first number is number then =>js consider first opreation  and combiend them.
+console.log(+true)//if + opreation first then true then => (js add 0+ (true) (true consider 1) )=1 
+// console.log(true+)// if + opreation after true then js show error
+let k=2
+++k; //prefix opreater
+console.log(k)
+/*
+    prefix opreater
+    increment before opretor
+*/
+k++;
+console.log(k);
+/*
+postfix opreator
+increment after opretor
+*/
+// help line :-https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
